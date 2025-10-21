40fd5823cc67013da95e330526983e7f2d59aa9d
Vinicius Fernandes <viniciusclementefernandes@gmail.com>
2025-10-21 14:29:57 -0300

    fix: update PDF download filename format in CartaControleDetailsPage for improved clarity

--- src/app/dashboard/cartas-controle/[id]/page.tsx
+++ src/app/dashboard/cartas-controle/[id]/page.tsx
@@ -69,7 +69,7 @@ export default function CartaControleDetailsPage() {
     const url = URL.createObjectURL(blob);
     const a = document.createElement('a');
     a.href = url;
-    a.download = `carta-controle-${cartaControle.remessa}-${dayjs(
+    a.download = `DT ${cartaControle.documentoTransporte} ${dayjs(
       cartaControle.dataIdentificacao,
     ).format('DD-MM-YYYY')}.pdf`;
     document.body.appendChild(a);
